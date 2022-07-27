4e077fe251c44613704d63ccd678a415da795b6f
Mr.chu <chuhongkyu@gmail.com>
2022-07-27 17:09:07 +0900

    차트 수정

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,28 +1,25 @@
-const BASE_URL = `https://api.coinpaprika.com/v1`
+const BASE_URL = `https://api.coinpaprika.com/v1`;
 
-export async function fetchCoins(){
-    return fetch(`${BASE_URL}/coins`).then((response) => 
-    response.json()
-    )
+export async function fetchCoins() {
+  return fetch(`${BASE_URL}/coins`).then((response) => response.json());
 }
 
-export function fetchCoinInfo(coinId:string){
-    return fetch(`${BASE_URL}/coins/${coinId}`).then((response) => 
+export function fetchCoinInfo(coinId: string) {
+  return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
     response.json()
-    )
+  );
 }
 
-export function fetchCoinTickers(coinId:string){
-    return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) => 
+export function fetchCoinTickers(coinId: string) {
+  return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
     response.json()
-    )
+  );
 }
 
-export function fetchCoinHistory(coinId:string){
-    const endDate = Math.floor(Date.now() / 1000);
-    const startDate = endDate - 60 * 60 * 24 * 7 * 2;//
-    return fetch
-    (`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`).then((response) => 
+export function fetchCoinHistory(coinId: string) {
+  // const endDate = Math.floor(Date.now() / 1000);
+  // const startDate = endDate - 60 * 60 * 24 * 7 * 2;//
+  return fetch(`${BASE_URL}/coins/${coinId}/ohlcv/today`).then((response) =>
     response.json()
-    )
+  );
 }
